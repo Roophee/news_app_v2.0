8c9b903f4d2849ca90932a8ad3e642a8c7ba44f3
Roophee <alex.babaew@gmail.com>
2021-06-13 20:33:05 +0300

    fix resetButton effect - setNewsStorage(null)

--- src/hoc/QueryStateProvider.js
+++ src/hoc/QueryStateProvider.js
@@ -78,7 +78,7 @@ export default function QueryStateProvider(props) {
 
   useEffect(() => {
     if (resetWasClicked) {
-      setNewsInStorage([]);
+      setNewsInStorage(null);
       dispatch({ type: 'RESET' });
       setResetWasClicked(false);
     }
