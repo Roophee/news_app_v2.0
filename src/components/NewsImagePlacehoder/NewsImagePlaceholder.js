e5241a42bf21f87922b5adb888348b192c1faca9
Roophee <alex.babaew@gmail.com>
2021-06-14 19:14:40 +0300

    fix ImagePlaceholder, set Loading image, onError NoImage

--- src/components/NewsImagePlacehoder/NewsImagePlaceholder.js
+++ src/components/NewsImagePlacehoder/NewsImagePlaceholder.js
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
-import { getUrlForNewsImage } from '../../data/dataHandlers';
-import NoImage from '../../images/noImage.png';
+import styled from 'styled-components';
+import { getUrlForNewsImage, returnPlaceHolderUrl } from '../../data/dataHandlers';
+import LoadingImage from '../../images/LoadingImage.png';
+
+const StyledImagePlaceHolder = styled.img`
+  display: ${props => (props.display ? 'block' : 'none')};
+`;
 
 const NewsImagePlaceholder = props => {
   const { media, clean_url } = props;
-  const [isLoading, setIsLoading] = useState(false);
+  const [downloadStatus, setDownloadStatus] = useState(true);
   const [imageLoadError, setImageLoadError] = useState(true);
 
   const onLoadHandler = () => {
-    setIsLoading(false);
+    setDownloadStatus(false);
   };
 
   const onErrorHandler = e => {
     if (imageLoadError) {
-      e.target.src = 0;
+      e.target.src = `https://via.placeholder.com/450x250.png/F5F5F5/d32f2f?text=No Image`;
       setImageLoadError(false);
     }
   };
+
   return (
     <>
-      {isLoading && NoImage}
-      <img
+      <StyledImagePlaceHolder
+        display={downloadStatus}
+        src={LoadingImage}
+        className="news__picture"
+        alt="News_photo"
+      />
+
+      <StyledImagePlaceHolder
+        display={!downloadStatus}
         className="news__picture"
         alt="News_photo"
         src={getUrlForNewsImage(media, clean_url)}
